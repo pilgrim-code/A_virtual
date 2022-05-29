fd70d60f60529d0b201e4a1b7c8e7aad2d2f506b
Lukas <81888309+pilgrim-code@users.noreply.github.com>
2022-05-29 13:25:14 -0400

    Main Primitivo con requirements basicos
    Deberia hacer una CNN clasificadora de audios en tiempo real para mejorar la respuesta

---
+++ main.py
@@ -0,0 +1,108 @@
+import pyttsx3 
+import speech_recognition as sr 
+import datetime
+import webbrowser
+import os
+import smtplib
+#import tensorflow as tf
+opera_path = r"C://Users//Pilgrim//AppData//Local//Programs//Opera GX//opera.exe"
+webbrowser.register('opera',None,webbrowser.BackgroundBrowser(opera_path),preferred=True)
+
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+# print(voices[1].id)
+engine.setProperty('voice', voices[0].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def Inicio():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Buenos dias!")
+
+    elif hour>=12 and hour<18:
+        speak("Buenas tardes!")   
+
+    else:
+        speak("Buenas noches!")  
+
+    speak("Soy tu asistente dime un comando para ayudarte!")       
+
+def takeCommand():
+    #It takes microphone input from the user and returns string output
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")    
+        query = r.recognize_google(audio, language='es-ES')
+        speak(f"Dijo: {query}\n")
+        print(f"FLAG {query}\n")
+        if query == "arroba":
+            query = "@"+""
+            print(query)
+
+    except Exception as e:
+        # print(e)    
+        speak("Intenta nuevamente...")  
+        return "None"
+    return query
+
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login('youremail@gmail.com', 'your-password')
+    server.sendmail('youremail@gmail.com', to, content)
+    server.close()
+
+if __name__ == "__main__":
+    Inicio()
+    while True:
+    # if 1:
+        query = takeCommand().lower()
+
+        # Logic for executing tasks based on query
+     
+        if 'abrir youtube' in query:
+            webbrowser.get('opera').open("youtube.com")
+        
+        elif 'abrir historial youtube' in query:
+            webbrowser.get('opera').open("youtube.com/feed/history")
+
+        elif 'busca mis monas chinas' in query:
+            webbrowser.get('opera').open("https://www1.otakustv.com")
+
+        elif 'que hora es' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")    
+            speak(f"Señor, la hora es {strTime} ")
+
+        elif 'abrir visual' in query:
+            codePath = "C:/Users/Pilgrim/AppData/Local/Programs/Microsoft VS Code/Code.exe"
+            os.startfile(codePath)
+        
+        elif 'busca' in query:
+            speak("Que busco?")
+            query=webbrowser.get('opera').open(takeCommand()) 
+
+        elif 'mensaje a big data' in query:
+            try:
+                speak("que le digo?")
+                content = takeCommand()
+
+                
+                to = "harryyourEmail@gmail.com"    
+                sendEmail(to, content)
+                speak("Email has been sent!")
+            except Exception as e:
+                print(e)
+                speak("Sorry my friend harry bhai. I am not able to send this email")    
